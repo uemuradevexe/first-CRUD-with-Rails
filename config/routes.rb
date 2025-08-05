7755b674cac6addddb43f0ae811511663b8ed9b8
Ricardo Uemura <uemuradev.exe@gmail.com>
2025-08-05 15:16:11 +0900

    rubocop test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "home/index"
 
-  get '/usuarios', to: 'users#index'
+  get "/usuarios", to: "users#index"
 
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
+   # Defines the root path route ("/")
    root "home#index"
 end
